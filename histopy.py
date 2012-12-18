80a82ecb631ee00f4a8d4bc7ab964ce39a5a83de
Keheliya Gallaba <keheliya.gallaba@gmail.com>
2012-12-18 19:39:36 +0530

    code cleanup & API docs

--- histopy.py
+++ histopy.py
@@ -11,24 +11,24 @@ import unicodedata
 # now = datetime.datetime.now()
 # today = now.strftime("%B_%d")
 
-opener = urllib2.build_opener()
-opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+_opener = urllib2.build_opener()
+_opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 
  
-def remove_html_tags(html):
+def _remove_html_tags(html):
     p = re.compile(r'<[^<]*?/?>')
     return p.sub('', html)
 
 def load_history(someday):
     formatted_date = someday.strftime("%B_%d")
-    html = opener.open('http://en.wikipedia.org/wiki/'+formatted_date).read()
-    soup = BSoup(html)
-    return soup
+    html = _opener.open('http://en.wikipedia.org/wiki/'+formatted_date).read()
+    history = BSoup(html)
+    return history
 
-def load_ul(li,soup):
+def _load_ul(li,soup):
     item_dict = {}
     for li in soup.html.body.findAll('ul')[li]:
-        s = remove_html_tags(str(li))        
+        s = _remove_html_tags(str(li))        
         try:
             if int(s[0]) > 0:                
                 line = s.split('\xe2\x80\x93')
@@ -45,11 +45,11 @@ def load_ul(li,soup):
 
 
 
-def load_events(soup):
-    return load_ul(1,soup)
+def load_events(loaded_history):
+    return _load_ul(1,loaded_history)
 
-def load_births(soup):
-    return load_ul(2,soup)
+def load_births(loaded_history):
+    return _load_ul(2,loaded_history)
 
-def load_deaths(soup):
-    return load_ul(3,soup)
+def load_deaths(loaded_history):
+    return _load_ul(3,loaded_history)
